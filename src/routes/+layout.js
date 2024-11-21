f20ceb2fc4084fe0b150da18de84d373998dce2d
Highscoretech <highscoretechng@gmail.com>
2024-11-21 06:57:47 -0600

    FLieps I

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,21 +1,21 @@
-// import { browser } from '$app/environment';
-// import { AppScript } from "$lib/index.js"
-// import { app } from "$lib/store/app.js"
+import { browser } from '$app/environment';
+import { AppScript } from "$lib/index.js"
+import { app } from "$lib/store/app.js"
 
-// /** @type {import('./$types').PageLoad} */
-// export async function load({ route }) {
-//     const _apiScript = new AppScript()
-//     if(browser){
-//         setAPI_url(_apiScript )
-//     }
-//     return 
-// }
+/** @type {import('./$types').PageLoad} */
+export async function load({ route }) {
+    const _apiScript = new AppScript()
+    if(browser){
+        setAPI_url(_apiScript )
+    }
+    return 
+}
 
-// const setAPI_url = ((_apiScript)=>{
-//     let localhostUrl = "http://localhost:8000"
-//     let remoteUrl = "https://cat3poker-d07112c3d84c.herokuapp.com"
-//     const _api = location.hostname === "localhost" || location.hostname === "127.0.0.1"
-//     ? localhostUrl : remoteUrl
-//     _apiScript.backend_url(_api) 
-//     app.set(_apiScript)
-// })
+const setAPI_url = ((_apiScript)=>{
+    let localhostUrl = "http://localhost:8000"
+    let remoteUrl = "https://cat3poker-d07112c3d84c.herokuapp.com"
+    const _api = location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? localhostUrl : remoteUrl
+    _apiScript.backend_url(_api) 
+    app.set(_apiScript)
+})
