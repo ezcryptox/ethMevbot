11d1f433437c58e1ad8d9851281cf485963dc077
Highscoretech <highscoretechng@gmail.com>
2024-11-21 06:55:20 -0600

    try 1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,33 @@
 <script>
     import "../style/global.css"
-    // import Navbar from "$lib/navbar.svelte";
-    // import Nested from "$lib/component/wallet/nested.layout.svelte";
-    // import { page } from "$app/stores";
-    // import { searchUrl } from "$lib/store/app.js";
-    // export let data
+    import Navbar from "$lib/navbar.svelte";
+    import Nested from "$lib/component/wallet/nested.layout.svelte";
+    import { page } from "$app/stores";
+    import { searchUrl } from "$lib/store/app.js";
+    export let data
 
-    // $: queryString = ""
-    // $: params = ""
-    // $: paramsObj = {}
-    // $: {
-    //     queryString = $page.url.search
-    //     params = new URLSearchParams(queryString);
-    //     params.forEach((value, key) => {
-    //         paramsObj[key] = value;
-    //     });
-    //     searchUrl.set(paramsObj)
-    // }
-    // $: show = false
+    $: queryString = ""
+    $: params = ""
+    $: paramsObj = {}
+    $: {
+        queryString = $page.url.search
+        params = new URLSearchParams(queryString);
+        params.forEach((value, key) => {
+            paramsObj[key] = value;
+        });
+        searchUrl.set(paramsObj)
+    }
+    $: show = false
 </script>
 
 <div id="root">
     <div class="min-h-screen flex flex-col">
-        <!-- <Navbar isLogin={data?.session}/> -->
+        <Navbar isLogin={data?.session}/>
         <slot></slot>
     </div>
 </div>
-<!-- {#if  queryString}
+{#if  queryString}
     <Nested />
-{/if} -->
+{/if}
 
 
