6e89c354c6fce710d575134cabd0cbe1e0c427f1
Highscoretech <highscoretechng@gmail.com>
2024-11-22 23:02:26 -0600

    check1

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,11 +1,11 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Google from "@auth/sveltekit/providers/google";
-import { GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private";
+import { GOOGLEID, GOOGLE_SECRET } from "$env/static/private";
 
 
 // let GOOGLE_ID="821125452458-epkov7kh117grbql6se2md0aljcg674k.apps.googleusercontent.com"
 // let GOOGLE_SECRET="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"
 
 export const {handle, signIn } = SvelteKitAuth({
-    providers:[Google({clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET})]
+    providers:[Google({clientId: GOOGLEID, clientSecret: GOOGLE_SECRET})]
 })
