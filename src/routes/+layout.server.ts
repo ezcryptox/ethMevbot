e1752160292e80fdfd91124f1a97e393539ca1da
Highscoretech <highscoretechng@gmail.com>
2024-11-23 00:15:20 -0600

    ver4

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,9 @@
 import type { LayoutServerLoad } from "./$types.js"
 
 export const load: LayoutServerLoad = async (event) => {
-  // const session = await event.locals.auth()
+  console.log(event)
+  const session = await event.locals.auth()
+  console.log(session)
   // return { session }
 }
 
