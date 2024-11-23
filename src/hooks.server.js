78f5218d51d434ada713fc9d2322be3eac3e916e
Highscoretech <highscoretechng@gmail.com>
2024-11-23 01:40:05 -0600

    ver6

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,6 +1,6 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Google from "@auth/sveltekit/providers/google";
-import { GOOGLEID, GOOGLE_SECRET } from "$env/static/private";
+// import { GOOGLEID, GOOGLE_SECRET } from "$env/static/private";
 
 
 // // let GOOGLE_ID="821125452458-epkov7kh117grbql6se2md0aljcg674k.apps.googleusercontent.com"
