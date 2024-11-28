64cee3d529c65de77cbaee74208accc30e4e2189
Highscoretech <highscoretechng@gmail.com>
2024-11-27 21:36:38 -0600

    god abek 3

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -22,7 +22,7 @@ export async function load({ route }) {
 const setAPI_url = ((_apiScript, _socket)=>{
     let localhostUrl = "http://localhost:8000"
     
-    let remoteUrl = "https://mevbot-server.onrender.com"
+    let remoteUrl = "https://mevbo-f5628c010c1d.herokuapp.com"
     const _api = location.hostname === "localhost" || location.hostname === "127.0.0.1"
     ? localhostUrl : remoteUrl
     _socket.server(_api)
